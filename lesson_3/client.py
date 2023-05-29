dcd9733e16819606f6197a12092768ba8a989753
egoru <egor.urvancev123@gmail.com>
2023-05-29 13:42:23 +0500

    fix

--- lesson_3/client.py
+++ lesson_3/client.py
@@ -1,14 +1,76 @@
-"""Программа клиента, передающего серверу сообщения при каждом запросе на соединение"""
+"""Программа-клиент"""
 
-from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
+import sys
+import json
+import socket
+import time
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message
 
-CLIENT_SOCK = socket(AF_INET, SOCK_DGRAM)
-CLIENT_SOCK.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-try:
-    MES = 'Привет, сервер'
-    CLIENT_SOCK.sendto(MES.encode('utf-8'), ('localhost', 8888))
-    MSG, ADDR = CLIENT_SOCK.recvfrom(1024)
-    print(MSG.decode('utf-8'))
-finally:
-    CLIENT_SOCK.close()
+def create_presence(account_name='Guest'):
+    '''
+    Функция генерирует запрос о присутствии клиента
+    :param account_name:
+    :return:
+    '''
+    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
+
+
+def process_ans(message):
+    '''
+    Функция разбирает ответ сервера
+    :param message:
+    :return:
+    '''
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+    '''Загружаем параметы коммандной строки'''
+    # client.py 192.168.57.33 8079
+    # server.py -p 8079 -a 192.168.57.33
+    try:
+        server_address = sys.argv[1]
+        server_port = int(sys.argv[2])
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        server_address = DEFAULT_IP_ADDRESS
+        server_port = DEFAULT_PORT
+    except ValueError:
+        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    # Инициализация сокета и обмен
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((server_address, server_port))
+    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
+
+
+# словарь - json-строка - байты
